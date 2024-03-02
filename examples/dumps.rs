4482d402a8a280095ad2a7ddb683df9e8bb926cf
ranafge <ranafge@gmail.com>
2024-03-02 23:57:34 +0600

    inline attribue added

--- examples/dumps.rs
+++ examples/dumps.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello FROM DUPS")
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use std::sync::Arc;
+    use tokio::sync::Barrier;
+    #[inline(never)]
+    async fn a(barrier:Arc<Barrier>) {
+        b(barrier).await
+    }
+    #[inline(never)]
+    async fn b(barrier: Arc<Barrier>){
+        c(barrier).await
+    }
+
+    #[inline(never)]
+    async fn c(barrier: Arc<Barrier>){
+        barrier.wait().await;
+    }
+    async fn dump_or_quit() {
+        use tokio::time::{timeout, Duration, Instant};
+        let handle = tokio::runtime::Handle::current()
+    }
+
+    Ok(())
 }
